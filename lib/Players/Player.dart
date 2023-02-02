43f0cbd4ff1f46064befa0ce59f38279a6991971
CarlosAbaloC <carlos.abalocordero@gmail.com>
2023-02-02 23:30:30 +0100

    Creacion Pantalla

---
+++ lib/Players/Player.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flame/components.dart';
+import 'package:flame_forge2d/body_component.dart';
+import 'package:forge2d/src/dynamics/body.dart';
+import 'package:juego_actividad/Scenes/PantallaPrincipal.dart';
+
+class Player extends BodyComponent<PantallaPrincipal> {
+  @override
+  Body createBody() {
+    // TODO: implement createBody
+    throw UnimplementedError();
+  }
+
+
+}
--- lib/Scenes/PantallaPrincipal.dart
+++ lib/Scenes/PantallaPrincipal.dart
@@ -2,8 +2,37 @@
 
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_forge2d/forge2d_game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+
+import '../Objects/TileMapObjectBody.dart';
+import '../Players/Player.dart';
 
 class PantallaPrincipal extends Forge2DGame{
 
+
+  late TiledComponent mapComponent;
+
+  PantallaPrincipal():super(gravity: Vector2(0, 9.8), zoom: 1);
+
+  @override
+  Future<void>? onLoad() async {
+    // TODO: implement onLoad
+    await super.onLoad();
+
+    mapComponent = await TiledComponent.load('MapaC.tmx', Vector2.all(32));
+    add(mapComponent);
+
+    final capaObjGroup = mapComponent.tileMap.getLayer<ObjectGroup>('Objetos'); //Pide los objetos que estan en la capa objetos, los objetos con "cuerpo"
+
+    for(final obj in capaObjGroup!.objects) { //Un for para no ir objeto por objeto
+      TiledMapObjectBody tmob = TiledMapObjectBody(obj); //Pasa los objetos al tiled que los define en el mapa
+      add(tmob);
+    }
+
+    //Ubicacion de inicio del personaje
+    Player personaje = Player(); //size/2 es el centro de la pantalla
+    add(personaje);
+
+  }
 
 }
